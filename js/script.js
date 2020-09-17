3015ebcd09d8e124fd74b8f9827cf3a136b7d552
Dmirtiy <dimalie123@gmail.com>
2020-09-17 19:06:55 +0300

    lesson_2.if_&_ switch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+'use strict';
+
+// <first-lesson>-----------/
+
 const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
 
 const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
@@ -17,3 +21,36 @@ personalMovieDB.Movie[lastFilm] = lastFilmScore;
 personalMovieDB.Movie[lastFilm2] = lastFilmScore2; 
 
 console.log( personalMovieDB );
+
+// </first-lesson>-----------/
+
+// <second-lesson>-----------/
+
+    if ( 4 == 9 ) {
+        console.log('ok!');
+    }else {
+        console.log('not ok!');
+        
+    }
+
+// </second-lesson>-----------/
+
+const num = 50;
+
+( num == 50 ) ? console.log('ok!') : console.log("you're wrong");
+
+switch(num) {
+
+    case 49: 
+        console.log('неверно!');
+        break;
+    case 22:
+        console.log('неверно!');
+        break;    
+    case 50:
+        console.log('Правильно!');
+        break;    
+    default: 
+        console.log('в друной раз!');
+        break;   
+    }
