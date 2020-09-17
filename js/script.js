50c2a622fde92a5221f5ef45b5dffbf4b31aa0e9
Dmirtiy <dimalie123@gmail.com>
2020-09-17 14:32:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// let number = 5;
+
+// number = 10;
+
+// const persone = 'Alex';
+
+
+// const obj = {
+//     name: 'Dmitriy',
+//     age: '22',
+//     isMaried: false,
+// };
+
+// console.log(obj["name"]);
+
+// const conf = [];
+
+// conf[0] = prompt ("are you here?");
+// conf[1] = prompt ("how old are you?");
+// conf[2] = prompt ("are you maried?");
+
+// document.write(conf)
+
+console.log(15%10);
