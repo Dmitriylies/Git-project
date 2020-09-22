07868c1c7fe0864e60cc34cd3c612dd6c92a2b1a
Dmirtiy <dimalie123@gmail.com>
2020-09-22 20:18:36 +0300

    added css and main.js

--- js/main.js
+++ js/main.js
@@ -162,19 +162,99 @@
 
 
 
-const trupper = {
-    health: 100,
-    armor: 100,
-    hungry: true,
-};
-const nick = Object.create(trupper);
+// const trupper = {
+//     health: 100,
+//     armor: 100,
+//     hungry: true,
+// };
+// const nick = Object.create(trupper);
 
-nick.naked = false;
+// nick.naked = false;
  
-Object.setPrototypeOf( nick, trupper);
+// Object.setPrototypeOf( nick, trupper);
 
-nick.__proto__ = trupper;//устаревшая форма записи
+// nick.__proto__ = trupper;//устаревшая форма записи
 
-console.log(nick);
-console.log(nick.armor);
+// console.log(nick);
+// console.log(nick.armor);
 
+// 1+
+// 2-
+// 3-
+// 4-
+// 5-
+// 6
+// 7
+// 8
+// 9
+// 10
+
+// 1
+// let x = 5
+// console.log(x++);// 5 
+
+// 2
+//console.log([] + false - null + true);//NaN
+
+//3
+//let y = 1;
+//let x = y = 2;
+//console.log(x); //2
+
+//4
+//console.log([] + 1 + 2);// 12
+
+//5
+//console.log('1'[0]);// 1
+
+// 6
+
+//const box = document.getElementById('box'),
+//      buttons = document.getElementsByTagName('button'),
+//      circles = document.getElementsByClassName('circle'),
+//      wrapper = document.querySelector('.wrapper'),
+//      hearts = wrapper.querySelectorAll('.heart'),
+//      oneHeart = document.querySelector('.heart');
+//console.dir(box);
+
+//box.style.backgroundColor = '#255';
+//box.style.width = '500px';
+
+//buttons[1].style.borderRadius = '40px';
+//circles[2].style.backgroundColor = 'red';
+//box.style.cssText = 'background: blue; width: 800px;';
+//for (let i = 0; i < hearts.length; i++ ) {
+//    hearts[i].style.backgroundColor = 'blue';
+//}
+//hearts.forEach(item => {
+//    item.style.backgroundColor = 'blue';
+//});
+//const div = document.createElement('div');
+//const text = document.createTextNode('i was here');
+//
+//div.classList.add('black');
+//
+//document.body.append(div);// добавляет элемент в конец родителя
+//
+//wrapper.append(div );
+//wrapper.prepend(div);
+
+//hearts[2].before(div);
+//hearts[2].after(div);
+//circles[0].remove();
+//hearts[0].replaceWith(circles[0]);
+
+//wrapper.appendChild(div);// добавляет элемент в конец родителя
+//wrapper.insertBefore(div, hearts[0]);// добавляем элемент(1 аргумент) перед как-либо тегом(2 аргумент)
+//wrapper.removeChild(hearts[1]);// удаляет конкретный элемент
+//wrapper.replaceChild(circles[0], hearts[0] );// заменяет элемент 
+
+//div.innerHTML = '<h1>Hello world</h1>';// позволяет вставить HTML  структуру
+//div.textContent = 'Hello-Hello!';// добавляет текст на страницу, но HTML структура уже не читается  
+
+div.insertAdjacentHTML('beforebegin', '<h2>hello</h2>');//вставляет HTML перед элементом
+div.insertAdjacentHTML('afterend', '<h2>hello</h2>');//вставляет HTML после элемента
+div.insertAdjacentHTML('afterbegin', '<h2>hello</h2>');//вставляет HTML в начало элемента
+div.insertAdjacentHTML('beforeend', '<h2>hello</h2>');//вставляет HTML в конец элемента
+
+ 
--- js/script.js
+++ js/script.js
@@ -2,65 +2,73 @@
 
 let numberOfFilms;
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     Movie: {},
     actors: {},
     geners: [],
-    privat:  false,
+    privat:  true,
+    start: function start(){
+        this.count = +prompt('сколько фильмов вы уже посмотрели?', '');
+      
+        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+            this.count = +prompt('сколько фильмов вы уже посмотрели?', ''); 
+        }
+    },
+    detectPersLvl: function detectPersLvl() {
+        if (this.count < 10) {
+            confirm('просмотренно довольно мало фильмов!');
+        } else if (this.count >= 10 && this.count < 30) {
+            confirm('просмотренно достаточно фильмов!');
+        } else if (this.count > 30) {
+            confirm('Киноман!!');
+        } else {
+            confirm('error!');
+        }
+    },
+    rememberMyFilms: function rememberMyFilms() {
+        for (let i = 0; i < 2; i++) {
+            const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+                lastFilmScore = +prompt('Насколько оцените его?', '');
+        
+            if (lastFilm != null && lastFilm != '' && lastFilm.length < 50 && lastFilmScore != null && lastFilmScore != '') {
+                personalMovieDB.Movie[lastFilm] = lastFilmScore;
+                console.log('done');
+        
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    toggleVisibleMyDB: function toggleVisibleMyDB() {
+        if(this.privat) {
+            this.privat = false;
+        }else{
+            this.privat = true;
+        }
+    },
+    showMyDB: function showMyDB() {
+        if (!this.privat) {
+              console.log(personalMovieDB);
+      }
+    },
+    writeYourGeners: function writeYourGeners() {
+        for(let i = 1; i <= 3; i++ ){
+            this.geners[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+            if( this.geners[i - 1] == null || this.geners[i - 1] == '') {
+                i--;
+            }
+        }
+        this.geners.forEach (function(item, i) {
+            console.log(`любимый жанр №${i + 1} это ${item}`);
+        });
+    },
 };
 
-function start(){
-  numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
-
-  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
-    numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', ''); 
-  }
-}
-//start();
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-            lastFilmScore = +prompt('Насколько оцените его?', '');
-    
-        if (lastFilm != null && lastFilm != '' && lastFilm.length < 50 && lastFilmScore != null && lastFilmScore != '') {
-            personalMovieDB.Movie[lastFilm] = lastFilmScore;
-            console.log('done');
-    
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-//rememberMyFilms();
-
-
-function detectPersLvl() {
-    if (personalMovieDB.count < 10) {
-        confirm('просмотренно довольно мало фильмов!');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        confirm('просмотренно достаточно фильмов!');
-    } else if (personalMovieDB.count > 30) {
-        confirm('Киноман!!');
-    } else {
-        confirm('error!');
-    }
-}
-//detectPersLvl();
-
-function showMyDB(hidden) {
-      if (!hidden) {
-            console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGeners() {
-    for(let i = 1; i <= 3; i++ ){
-        personalMovieDB.geners[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-//writeYourGeners();
+//personalMovieDB.start();
+//personalMovieDB.detectPersLvl();
+//personalMovieDB.rememberMyFilms();
+//personalMovieDB.toggleVisibleMyDB();
+//personalMovieDB.showMyDB(personalMovieDB.privat);
+//personalMovieDB.writeYourGeners();
 
